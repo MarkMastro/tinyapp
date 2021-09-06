3b1aa35fab9f6721904cf55e0940f002e4552ca7
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-09-06 19:35:03 +0000

    add table for shortened urls and button for new one

--- express_server.js
+++ express_server.js
@@ -2,13 +2,29 @@ const express=require('express');
 const app=express();
 const PORT=8080;
 
+app.set('view engine','ejs')
+
 const urlDatabase={
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
 
 app.get("/",(req,res)=>{
-  res.send("hello ")
+ // res.render()
+});
+
+app.get("/urls.json", (req,res)=>{
+  res.json(urlDatabase);
+})
+
+app.get("/urls",(req,res)=>{
+  const templateVars={urls: urlDatabase};
+  res.render("urls_index",templateVars)
+})
+
+app.get("/hello", (req,res)=>{
+  const templateVars={greeting:'Hello World!'};
+  res.render("hello_world",templateVars)
 });
 
 app.listen(PORT,()=>{
