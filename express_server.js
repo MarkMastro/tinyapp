3b49f51b378c3eb4d04abbdbdf097d443473634a
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-09-07 23:11:52 +0000

    added redirect to longurl

--- express_server.js
+++ express_server.js
@@ -37,7 +37,9 @@ app.get("/urls",(req,res)=>{
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  randomLetters=randomFunction()
+  urlDatabase[randomLetters]=req.body.longURL
+  res.redirect(`/urls/${randomLetters}`)       // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls_new",(req,res)=>{
@@ -48,9 +50,13 @@ app.get("/hello", (req,res)=>{
   const templateVars={greeting:'Hello World!'};
   res.render("hello_world",templateVars)
 });
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
 
 app.get("/urls/:shortURL", (req, res) => {
-  
+  console.log("/urls/:shorturl");
   const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] };
   console.log(templateVars)
   res.render("urls_show", templateVars);
