d462e2432d289f56067edd032bfa074f40736a82
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-09-09 01:12:14 +0000

    add comments and eslint

--- express_server.js
+++ express_server.js
@@ -1,100 +1,94 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const bodyParser = require("body-parser");
-const cookieParser=require('cookie-parser')
-app.use(cookieParser())
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
-const PORT=8080;
+const PORT = 8080;
 
-app.set('view engine','ejs')
+app.set('view engine','ejs');
 
-const randomFunction=()=>{
-  var result = '';
-  var characters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < 6; i++ ) {
+//function to create set of 6 random letters to be our shortened link
+const randomFunction = ()=>{
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; //allowed characters in short url
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
- }
- return result; 
-}
+  }
+  return result;
+};
 
+//global "database" to be used by each endpoint 
+const urlDatabase = {
 
-const urlDatabase={
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-}
-
-app.get("/",(req,res)=>{
- // res.render()
-});
+};
 
 app.get("/urls.json", (req,res)=>{
   res.json(urlDatabase);
-})
+});
 
+//show all urls
 app.get("/urls",(req,res)=>{
-  const templateVars={
+  const templateVars = {
     username:req.cookies["username"],
     urls: urlDatabase
   };
-  res.render("urls_index",templateVars)
-})
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  if(req.body.longURL)
-  randomLetters=randomFunction()
-  urlDatabase[randomLetters]=req.body.longURL
-  res.redirect(`/urls/${randomLetters}`)       // Respond with 'Ok' (we will replace this)
+  res.render("urls_index",templateVars);
 });
 
+//create shortened url:long url and add to urldatabase
+//long url will be what is submitted from the form on urls_new.ejs
+app.post("/urls", (req, res) => {
+  let randomLetters = randomFunction();
+  urlDatabase[randomLetters] = req.body.longURL;
+  res.redirect(`/urls/${randomLetters}`);    
+});
+//display endpoint to enter new url
 app.get("/urls_new",(req,res)=>{
-  templateVars={
+  let templateVars = {
     username:req.cookies["username"]
-  }
-  res.render("urls_new")
-})
-
+  };
+  res.render("urls_new",templateVars);
+});
+//redirect GETs of the shortened url to the proper longurl
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
-
+//log the user in
 app.post("/login", (req,res)=>{
   res.cookie('username', req.body.username);
-  console.log(res.cookie);
-  res.redirect("/urls")
-})
-
+  res.redirect("/urls");
+});
+//log the user out and bring them back to main page
 app.post("/logout",(req,res)=>{
   res.clearCookie('username');
-  res.redirect("/urls")
-})
-
+  res.redirect("/urls");
+});
+//delete a url when button is pressed
 app.post("/urls/:shortURL/delete",(req,res)=>{
   delete urlDatabase[req.params.shortURL];
-  res.redirect('/urls')
-})
-
+  res.redirect('/urls');
+});
+//show the shortened url page
 app.get("/urls/:shortURL", (req, res) => {
-  console.log("/urls/:shorturl");
-  const templateVars = { 
+  const templateVars = {
     username:req.cookies["username"],
-    shortURL: req.params.shortURL, 
-    longURL:urlDatabase[req.params.shortURL] 
+    shortURL: req.params.shortURL,
+    longURL:urlDatabase[req.params.shortURL]
   };
   
-  console.log(templateVars)
   res.render("urls_show", templateVars);
 });
-
+//update url
 app.post("/urls/:shortURL", (req,res)=>{
-  if(req.params.shortURL){
-    urlDatabase[req.params.shortURL]=req.body.longURL
+  if (req.params.shortURL) {
+    urlDatabase[req.params.shortURL] = req.body.longURL;
   }
-  console.log(urlDatabase[req.params.shortURL],req.body.longURL)
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
+//turn server on
 app.listen(PORT,()=>{
-  console.log("server listening ")
+  console.log("server listening ");
 });
