737148a7f869f6b461d7b7fe36d59e190cd27ac5
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-09-11 16:50:00 +0000

    added tests

---
+++ test/helpersTest.js
@@ -0,0 +1,63 @@
+const { assert } = require('chai');
+
+const { emailLookup,urlsForUser,randomFunction } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+const testUrlDatabase = {
+  "sdfgfdsga": {
+    longUrl: "http://www.lighthouselabs.ca",
+    userID: "user1RandomID"
+  },
+  "asdf": {
+    longUrl: "http://www.google.com",
+    userID: "user1RandomID"
+  },
+  "asdfas": {
+    longUrl: "http://www.zara.com",
+    userID: "user2RandomID"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = emailLookup("user@example.com", testUsers)
+    const expectedOutput = "userRandomID";
+    // Write your assert statement here
+  });
+});
+
+describe('urlsForUser', function() {
+
+  it('should return an object of url information specific to the given user ID', function() {
+    const specificUrls = urlsForUser("user1RandomID", testUrlDatabase);
+    const expectedOutput = {
+      "sdfgfdsga": {
+        longUrl: "http://www.lighthouselabs.ca",
+        userID: "user1RandomID"
+      },
+      "asdf": {
+        longUrl: "http://www.google.com",
+        userID: "user1RandomID"
+      }
+    };
+    assert.deepEqual(specificUrls, expectedOutput);
+  });
+
+  it('should return an empty object if no urls exist for a given user ID', function() {
+    const noSpecificUrls = urlsForUser("fakeUser", testUrlDatabase);
+    const expectedOutput = {};
+    assert.deepEqual(noSpecificUrls, expectedOutput);
+  });
+});
